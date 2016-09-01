b40d89291eaa18df5ec9721214ebacc619bcda12
Martin Cocaro <martin.cocaro@gmail.com>
2016-09-01 14:46:38 -0300

    Fixing license error.

--- ringcaptcha.php
+++ ringcaptcha.php
@@ -7,7 +7,7 @@
  *
  * @package Ringcaptcha
  * @author  Cristian Iturri <iturri.cf@gmail.com>
- * @license http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License
+ * @license Apache 2.0
  * @link    http://ringcaptcha.com/
  */
 class Ringcaptcha
